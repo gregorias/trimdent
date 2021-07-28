2a3ec2b2d4a93cfba4586588181d10c9a6d29d4b
Grzegorz Milka <grzegorzmilka@gmail.com>
2021-07-28 10:36:04 +0200

    Implement trimdent

--- src/Trimdent.hs
+++ src/Trimdent.hs
@@ -1,8 +1,13 @@
+-- | A collection of trimming and unindenting utilities.
 module Trimdent (
   trim,
+  trimdent,
+  unindent,
 ) where
 
 import Data.Char (isSpace)
+import Data.List (sort)
+import Data.Maybe (listToMaybe)
 
 -- | Trims whitespace characters from both ends of the text.
 --
@@ -13,3 +18,36 @@ trim = dropWhileRev isSpace . dropWhile isSpace
 
 dropWhileRev :: (Char -> Bool) -> String -> String
 dropWhileRev p = reverse . dropWhile p . reverse
+
+-- | Smartly unindents a multiline text.
+--
+-- >>> unindent "\n  def f():\n    return 1\n"
+-- "\ndef f():\n  return 1\n"
+unindent :: String -> String
+unindent s =
+  case lines s of
+    sHead : sTail ->
+      let unindentedsHead = dropWhile (== ' ') sHead
+          minimumsTailIndent = minimumIndent . unlines $ sTail
+          unindentedsTail = case minimumsTailIndent of
+            Just indent -> map (drop indent) sTail
+            Nothing -> sTail
+       in unlines $ unindentedsHead : unindentedsTail
+    [] -> []
+ where
+  minimumIndent :: [Char] -> Maybe Int
+  minimumIndent =
+    listToMaybe . sort . map lineIndent
+      . filter (not . null . dropWhile isSpace)
+      . lines
+
+-- | Returns the amount of spaces on the first line.
+lineIndent :: [Char] -> Int
+lineIndent = length . takeWhile (== ' ')
+
+-- | Smartly unindents and trims a multiline text.
+--
+-- >>> trimdent "\n  def f():\n    return 1\n"
+-- "def f():\n  return 1"
+trimdent :: String -> String
+trimdent = trim . unindent
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,20 +2,33 @@ module Spec (main) where
 
 import Data.Char (isSpace)
 import Data.List (isInfixOf)
-import Test.Hspec (describe)
+import Test.Hspec (describe, it)
 import qualified Test.Hspec as Hspec
-import Test.Hspec.QuickCheck (prop)
-import Trimdent (trim)
+import Test.Hspec.Expectations.Pretty (shouldBe)
+import Test.Hspec.QuickCheck (modifyMaxSuccess, prop)
+import Text.RawString.QQ (r)
+import Trimdent (trim, trimdent)
 
 main :: IO ()
 main = Hspec.hspec tests
 
 tests :: Hspec.Spec
-tests = do
+tests = modifyMaxSuccess (const 1000) $ do
   describe "trim" $ do
-    prop "preservesContent" prop_trim_preservesContent
-    prop "commutesWithReverse" prop_trim_commutesWithReverse
-    prop "resultDoesntStartWithWhitespace" prop_trim_removesStartSpace
+    prop "preserves content" prop_trim_preservesContent
+    prop "commutes with reverse" prop_trim_commutesWithReverse
+    prop "result doesnt start with whitespace" prop_trim_removesStartSpace
+  describe "trimdent" $ do
+    prop "preserves nonwhitespace content" prop_trimdent_preservesNonwhitespaceContent
+    prop "result doesnt start with whitespace" prop_trimdent_removesStartSpace
+    it "trimdents the README string" itTrimdentsReadmeString
+    it "trimdents a headerless string" itTrimdentsHeaderlessString
+
+prop_f_removesStartSpace :: (String -> String) -> String -> Bool
+prop_f_removesStartSpace f s =
+  case f s of
+    "" -> True
+    c : _ -> not $ isSpace c
 
 prop_trim_preservesContent :: String -> Bool
 prop_trim_preservesContent s = trim s `isInfixOf` s
@@ -24,7 +37,36 @@ prop_trim_commutesWithReverse :: String -> Bool
 prop_trim_commutesWithReverse s = (trim . reverse) s == (reverse . trim) s
 
 prop_trim_removesStartSpace :: String -> Bool
-prop_trim_removesStartSpace s =
-  case trim s of
-    "" -> True
-    c : _ -> not $ isSpace c
+prop_trim_removesStartSpace = prop_f_removesStartSpace trim
+
+prop_trimdent_preservesNonwhitespaceContent :: String -> Bool
+prop_trimdent_preservesNonwhitespaceContent s =
+  dropWhitespace (trimdent s)
+    `isInfixOf` dropWhitespace s
+ where
+  dropWhitespace = filter (not . isSpace)
+
+prop_trimdent_removesStartSpace :: String -> Bool
+prop_trimdent_removesStartSpace = prop_f_removesStartSpace trimdent
+
+itTrimdentsReadmeString :: Hspec.Expectation
+itTrimdentsReadmeString =
+  trimdent
+    [r|func add(x int, y int) int {
+                return x + y
+              }
+              |]
+    `shouldBe` "func add(x int, y int) int {\n\
+               \  return x + y\n\
+               \}"
+
+itTrimdentsHeaderlessString :: Hspec.Expectation
+itTrimdentsHeaderlessString =
+  trimdent
+    "\n\
+    \   func add(x int, y int) int {\n\
+    \     return x + y\n\
+    \   }"
+    `shouldBe` "func add(x int, y int) int {\n\
+               \  return x + y\n\
+               \}"
